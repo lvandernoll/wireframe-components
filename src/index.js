294de8d0962bce816b13be8413ce8a44366cd9b4
SHoogenboom <shoogenboom@competa.com>
2019-02-05 12:00:51 +0100

    removed nav

--- src/index.js
+++ src/index.js
@@ -1,12 +1,9 @@
 import React from 'react';
 import Section from './components/Section';
-import Nav from './components/Nav'
 import Headline from './components/Headline';
 import Subtitle from './components/Subtitle';
 
 export {
-	Section,
-	Nav,
 	Section,
 	Headline,
 	Subtitle,
